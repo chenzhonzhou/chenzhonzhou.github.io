158c5861d4124697211fa1c714353117322b5650
chenzhongzhou <>
2019-10-25 15:18:50 +0800

    Site updated: 2019-10-25 15:18:40

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Thu Oct 24 2019 17:19:26 GMT+0800 (GMT+08:00)
-!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
+// build time:Fri Oct 25 2019 15:18:34 GMT+0800 (GMT+08:00)
+!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
