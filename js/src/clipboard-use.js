e8edb84ddc6671bd0789e3d237448bc3aa412002
chenzhongzhou <>
2019-11-21 17:54:52 +0800

    Site updated: 2019-11-21 17:53:41

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Thu Nov 21 2019 16:55:11 GMT+0800 (GMT+08:00)
+// build time:Thu Nov 21 2019 17:49:54 GMT+0800 (GMT+08:00)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
