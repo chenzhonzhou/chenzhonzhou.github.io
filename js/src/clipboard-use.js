28b9020f27c3546f0b82132054817c7224b1cb85
chenzhongzhou <chenzhongzhou18@163.com>
2019-12-17 09:52:16 +0800

    Site updated: 2019-12-17 09:50:31

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,1 @@
-// build time:Sat Dec 14 2019 17:53:17 GMT+0800 (GMT+08:00)
-!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
-//rebuild by neat 
+window,document,$(".highlight .code pre").before('<button class="btn-copy" data-clipboard-snippet="">  <i class="fa fa-clipboard"></i><span>copy</span></button>'),new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}});
