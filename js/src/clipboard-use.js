56b0795e05791f823670547d62e90b2549582715
Your Name <you@e.com>
2021-09-13 12:50:42 +0800

    Site updated: 2021-09-13 12:50:33

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,1 +1,3 @@
-window,document,$(".highlight .code pre").before('<button class="btn-copy" data-clipboard-snippet="">  <i class="fa fa-clipboard"></i><span>copy</span></button>'),new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}});
+// build time:Mon Sep 13 2021 11:56:26 GMT+0800 (GMT+08:00)
+!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
+//rebuild by neat 
