7845053f5a120d8e3553fc7e1e60faa07f5ae10a
chenzhongzhou <chenzhongzhou@163.com>
2023-12-03 15:24:58 +0800

    Site updated: 2023-12-03 15:24:50

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,1 @@
-// build time:Sun Dec 03 2023 14:39:33 GMT+0800 (GMT+08:00)
-!function(n,t,i){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight").wrap($('<div class="highlight-wrap"></div>'));$(".highlight-wrap").prepend(n);var t=new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}});t.on("success",function(n){toastPlug("复制成功!",2e3)})};a()}(window,document);
-//rebuild by neat 
+window,document,$(".highlight").wrap($('<div class="highlight-wrap"></div>')),$(".highlight-wrap").prepend('<button class="btn-copy" data-clipboard-snippet="">  <i class="fa fa-globe"></i><span>copy</span></button>'),new ClipboardJS(".btn-copy",{target:function(t){return t.nextElementSibling}}).on("success",function(t){toastPlug("复制成功!",2e3)});
